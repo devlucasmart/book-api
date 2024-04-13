7dcba264ea1b3b084a6d97d6b0664020d214f767
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-04-13 16:16:53 -0300

    implementa regra de negocio. falta update e save

--- src/main/java/com/devlucasmart/book/service/BookService.java
+++ src/main/java/com/devlucasmart/book/service/BookService.java
@@ -1,10 +1,14 @@
 package com.devlucasmart.book.service;
 
+import com.devlucasmart.book.comum.exception.ValidacaoException;
 import com.devlucasmart.book.dto.book.BookRequest;
 import com.devlucasmart.book.dto.book.BookResponse;
+import com.devlucasmart.book.mappers.BookMapper;
+import com.devlucasmart.book.model.BookModel;
 import com.devlucasmart.book.repository.BookRepository;
 import com.devlucasmart.book.repository.CategoriaRepository;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,22 +18,33 @@ import java.util.List;
 public class BookService {
     private final BookRepository repository;
     private final CategoriaRepository categoriaRepository;
+    private BookMapper mapper = Mappers.getMapper(BookMapper.class);
 
-    public List<BookResponse> findAll(){
+
+    public List<BookResponse> findAll() {
+        var books = repository.findAll();
+        return mapper.toListResponse(books);
+    }
+
+    public BookResponse findById(Integer id) {
+        var book = getById(id);
+        return mapper.toResponse(book);
+    }
+
+    public BookResponse save(BookRequest request) {
         return null;
     }
 
-    public BookResponse findById(Integer id){
+    public BookResponse update(Integer id, BookRequest request) {
         return null;
     }
 
-    public BookResponse save(BookRequest request){
-        return null;
-    }
-    public BookResponse update(Integer id, BookRequest request){
-        return null;
+    public void delete(Integer id) {
+        var book = getById(id);
+        repository.deleteById(book.getId());
     }
 
-    public void delete(Integer id){
+    private BookModel getById(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ValidacaoException("Book não Encontrado!!"));
     }
 }
--- src/main/java/com/devlucasmart/book/service/CategoriaService.java
+++ src/main/java/com/devlucasmart/book/service/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.devlucasmart.book.service;
 
+import com.devlucasmart.book.comum.exception.ValidacaoException;
 import com.devlucasmart.book.dto.categoria.CategoriaRequest;
 import com.devlucasmart.book.dto.categoria.CategoriaResponse;
+import com.devlucasmart.book.mappers.CategoriaMapper;
+import com.devlucasmart.book.model.CategoriaModel;
 import com.devlucasmart.book.repository.CategoriaRepository;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,13 +16,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoriaService {
     private final CategoriaRepository repository;
+    private CategoriaMapper mapper = Mappers.getMapper(CategoriaMapper.class);
+
 
     public List<CategoriaResponse> findAll(){
-        return null;
+        var categorias = repository.findAll();
+        return mapper.toListResponse(categorias);
     }
 
     public CategoriaResponse findById(Integer id){
-        return null;
+        var categoria = getById(id);
+        return mapper.toResponse(categoria);
     }
 
     public CategoriaResponse save(CategoriaRequest request){
@@ -28,6 +36,13 @@ public class CategoriaService {
         return null;
     }
 
+    //TODO Implementar validação se categoria estar vazia antes de deletar
     public void delete(Integer id){
+        var categoria = getById(id);
+        repository.deleteById(categoria.getId());
+    }
+
+    private CategoriaModel getById(Integer id){
+        return repository.findById(id).orElseThrow(() -> new ValidacaoException("Categoria não Encontrada!!"));
     }
 }
