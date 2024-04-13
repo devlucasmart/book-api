df7e89b85cb5d3b6d6e5fb6c5a8ff2338ff3bc8f
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-04-13 14:54:58 -0300

    initial commit

---
+++ src/test/java/com/devlucasmart/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
